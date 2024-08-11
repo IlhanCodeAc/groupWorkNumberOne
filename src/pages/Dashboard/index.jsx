0862dca0ee900dc211e5bdf42d1f186f3936bf93
Mammadova Shabnam <shabnamjm@code.local>
2024-08-11 11:28:51 +0400

    roting added

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Dasboard = () => {
+const Dashboard = () => {
   return (
     <div>
       Dashboard
@@ -8,4 +8,4 @@ const Dasboard = () => {
   )
 }
 
-export default Dasboard
+export default Dashboard
---
+++ src/routes/router.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import MainLayout from "../components/MainLayout";
+import Productos from "../pages/Productos";
+import Ventas from "../pages/Ventas";
+
+export const ROUTES = [
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Productos />,
+      },
+      {
+        path: "productos",
+        element: <Productos />,
+      },
+      {
+        path: "ventas",
+        element: <Ventas />,
+      },
+    ],
+  },
+];
