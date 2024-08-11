44bd73da0a4b46cb720b1cec89159dcfca288233
IlhanCodeAc <ilhanma@code.edu.az>
2024-08-11 17:49:34 +0400

    admin bar created

--- src/pages/AdminDashboard/index.jsx
+++ src/pages/AdminDashboard/index.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import Container from "../../components/Container";
+import styles from "./style.module.scss";
+import identIcon from "./imgs/identIcon.png";
+import BarChart from '../../components/BarChart'
 
 const AdminDashboard = () => {
   return (
-    <div>
-      AdminDashboard
-    </div>
-  )
-}
+    <>
+      <main className={styles.AdminMain}>
+        <Container>
+          <header className={styles.AdminHead}>
+            <div className={styles.AdminLeft}>
+              <img src={identIcon} alt="pfp" className={styles.pfp} />
+            </div>
+            <div className={styles.Right}>Admin</div>
+          </header>
+          <div className={styles.AdminOptions}>
+            <div className={styles.AddOption}>+ Add Product</div>
+            <div className={styles.Search}>Rates</div>
+            <div className={styles.Users}>Users</div>
+            <div className={styles.Orders}>Orders</div>
+          </div>
+        </Container>
+      </main>
+    </>
+  );
+};
 
-export default AdminDashboard
+export default AdminDashboard;
--- src/services/getProducts.js
+++ src/services/getProducts.js
@@ -1,7 +1,7 @@
-import axios from "axios"
-const BASE_URL = import.meta.env.VITE_BASE_URL
+import axios from "axios";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-export const getProducts = async()=>{
-    const response = await axios.get(`${BASE_URL}/products`)
-    return response.data
-}
+export const getProducts = async () => {
+  const response = await axios.get(`${BASE_URL}/products`);
+  return response.data;
+};
