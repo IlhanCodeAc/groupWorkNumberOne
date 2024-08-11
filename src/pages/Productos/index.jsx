229c4722d91ef644822587ea4f84bc299a0de32b
IlhanCodeAc <ilhanma@code.edu.az>
2024-08-11 12:02:59 +0400

    container prop created

--- src/pages/Productos/index.jsx
+++ src/pages/Productos/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Container from '../../components/Container'
 
 const Productos = () => {
   return (
-    <div>
-      Productos
-    </div>
+    <>
+    
+    </>
   )
 }
 
