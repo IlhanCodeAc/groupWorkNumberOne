93da2c4ba7cdc3cc5da8ee8eff636153431cb07d
Amrah.Garat <amrahshg@code.edu.az>
2024-08-11 16:42:03 +0400

    Footer added

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from "./index.module.scss"
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <h3>Short links</h3>
+      <ul className={styles["footer-list"]}>
+        <Link className={styles.link} to="dashboard">Dashboard</Link>
+        <Link className={styles.link} to="products">Products</Link>
+        <Link className={styles.link} to="ventas">Ventas</Link>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
