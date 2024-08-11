a534c44c4f34297ff03d1dd0b38c5d72500442e2
Mammadova Shabnam <shabnamjm@code.local>
2024-08-11 12:12:39 +0400

    header scss added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
+import styles from "./index.module.scss"
 const Header = () => {
   return (
-    <div>
-    <ul>
-        <li>Dashboard</li>
-        <li>Products</li>
-    </ul>
+    <div className={styles.header}> 
+      <ul>
+        <li>
+          <Link to='/'>Dashboard</Link>
+        </li>
+        <li>
+          <Link to='/productos'>Products</Link>
+        </li>
+        <li>
+          <Link to='/ventas'>Ventas</Link>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
 
 export default Header
