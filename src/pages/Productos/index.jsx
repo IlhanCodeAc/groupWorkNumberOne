7419423c2d7afef6d42a482a24d5f53febc4b62e
ulvi05 <aghazadeulvi@gmail.com>
2024-08-12 03:05:57 +0400

    features changes added

--- src/pages/Productos/index.jsx
+++ src/pages/Productos/index.jsx
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductsAction } from "../../redux/features/actions/productActions";
 import { MdDeleteOutline } from "react-icons/md";
-import { Button } from "@mui/material";
+import { Button, CircularProgress } from "@mui/material";
 import { deleteProductsAction } from "../../redux/features/actions/deleteAction";
 import { toggleDarkMode } from "../../redux/features/theme/themeSlice";
 import { FaRegMoon } from "react-icons/fa6";
@@ -14,7 +14,7 @@ import { FaMoon } from "react-icons/fa6";
 
 const Productos = () => {
   const dispatch = useDispatch();
-  const products = useSelector((state) => state.products.products);
+  const { products, loading, error } = useSelector((state) => state.products);
   const darkMode = useSelector((state) => state.theme.darkMode);
 
   useEffect(() => {
@@ -23,74 +23,86 @@ const Productos = () => {
 
   const handleDelete = (id) => {
     dispatch(deleteProductsAction({ id }));
- }
-
-const handleToggleDarkMode = () => {
-    dispatch(toggleDarkMode());
-    console.log('Dark Mode Toggled:', darkMode);
   };
 
+  const handleToggleDarkMode = () => {
+    dispatch(toggleDarkMode());
+    console.log("Dark Mode Toggled:", darkMode);
+  };
 
   return (
     <>
-    <div className={darkMode ? styles.darkMode : styles.lightMode}>
-      <Container>
-        <style>
-          @import
-          url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Tiny5&display=swap');
-        </style>
-        <main className={styles.productocontainer}>
-          <header className={styles.productoshead}>
-            <h3 className={styles.prodcutHeadText}>Available products</h3>
-            <Button onClick={handleToggleDarkMode} variant="contained" sx={{ marginBottom: '20px' }}>
-              {darkMode ? <FaMoon style={{fontSize:"30px"}} /> : <FaRegMoon style={{fontSize:"30px"}} />}
-            </Button>
-
-          </header>
-          <div className={styles.Cards}>
-            {products &&
-              products.map((data) => (
-                <div key={data.id} className={styles.card}>
-                  <div className={styles.cardImg}>
-                    <img src={data.category.image} alt={data.title} />
-                  </div>
-                  <div className={styles.cardFoot}>
-                    <div className={styles.FootHead}>
-                      {/* <h3 className={styles.ProductDesc}>
+      <div className={darkMode ? styles.darkMode : styles.lightMode}>
+        <Container>
+          <main className={styles.productocontainer}>
+            <header className={styles.productoshead}>
+              <h3 className={styles.prodcutHeadText}>Available products</h3>
+              <Button
+                onClick={handleToggleDarkMode}
+                variant="contained"
+                sx={{ marginBottom: "20px" }}
+              >
+                {darkMode ? (
+                  <FaMoon style={{ fontSize: "30px" }} />
+                ) : (
+                  <FaRegMoon style={{ fontSize: "30px" }} />
+                )}
+              </Button>
+            </header>
+            {loading ? (
+              <div className={styles.spinnerContainer}>
+                <CircularProgress style={{fontSize:"65px"}} />
+              </div>
+            ) : error ? (
+              <div className={styles.error}>{error}</div>
+            ) : (
+              <div className={styles.Cards}>
+                {products &&
+                  products.map((data) => (
+                    <div key={data.id} className={styles.card}>
+                      <div className={styles.cardImg}>
+                        <img src={data.category.image} alt={data.title} />
+                      </div>
+                      <div className={styles.cardFoot}>
+                        <div className={styles.FootHead}>
+                          {/* <h3 className={styles.ProductDesc}>
                         {data.description.length > 100
                           ? data.description.substring(0, 200) + "..."
                           : data.description}
                       </h3> */}
-                    </div>
-                    <div className={styles.FootBottom}>
-                      <div className={styles.NameCont}>
-                        <h4 className={styles.NameText}>{data.title}</h4>
+                        </div>
+                        <div className={styles.FootBottom}>
+                          <div className={styles.NameCont}>
+                            <h4 className={styles.NameText}>{data.title}</h4>
+                          </div>
+                          <div className={styles.Price}>
+                            <h3 className={styles.PriceText}>{data.price}$</h3>
+                          </div>
+                          <Button
+                            color="success"
+                            variant="contained"
+                            size="small"
+                          >
+                            <Link
+                              style={{ textDecoration: "none", color: "white" }}
+                              to={`/edit/${data.id}`}
+                            >
+                              Edit
+                            </Link>
+                          </Button>
+                          <MdDeleteOutline
+                            className={styles.delete}
+                            onClick={() => handleDelete(data.id)}
+                          />
+                        </div>
                       </div>
-                      <div className={styles.Price}>
-                        <h3 className={styles.PriceText}>{data.price}$</h3>
-                      </div>
-                      <Button color="success" variant="contained" size="small">
-                        <Link
-                          style={{ textDecoration: "none", color: "white" }}
-                          to={`/edit/${data.id}`}
-                        >
-                          Edit
-                        </Link>
-                      </Button>
-
-                      <MdDeleteOutline
-                        className={styles.delete}
-                        onClick={() => handleDelete(data.id)}
-                      />
                     </div>
-                  </div>
-                </div>
-              ))}
-          </div>
-        </main>
-      </Container>
-    </div>
-      
+                  ))}
+              </div>
+            )}
+          </main>
+        </Container>
+      </div>
     </>
   );
 };
--- src/redux/features/products/productSlice.js
+++ src/redux/features/products/productSlice.js
@@ -1,17 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { getProductsAction } from "../actions/productActions";
 
-
 const ProductSlice = createSlice({
   name: "product",
   initialState: {
     products: [],
+    loading: true,
+    error: null,
   },
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(getProductsAction.fulfilled, (state, action) => {
-      state.products = action.payload;
-    });
+    builder
+      .addCase(getProductsAction.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(getProductsAction.fulfilled, (state, action) => {
+        state.loading = false;
+        state.products = action.payload;
+      })
+      .addCase(getProductsAction.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
   },
 });
 
